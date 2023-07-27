e47fda0206b269b92899b8112062ca351d70d024
everinurmind <nurbol.sultanov@gmail.com>
2023-07-27 00:02:50 -0600

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get '/api/message', to: 'message#index', as: 'message'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'root#index'
+  get '*path', to: 'root#index'
 end
